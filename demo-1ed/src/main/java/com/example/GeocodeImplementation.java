b9d40c59e4c6fd806a36f74b10d6c3d880105161
darlesh <darleshpatil@gmail.com>
2017-04-28 18:51:18 +0530

    Update GeocodeImplementation.java

--- demo-1ed/src/main/java/com/example/GeocodeImplementation.java
+++ demo-1ed/src/main/java/com/example/GeocodeImplementation.java
@@ -1,30 +1,25 @@
 package com.example;
 
-	
-	import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-	import java.net.URLConnection;
-	import java.net.URLEncoder;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
-
 import org.apache.commons.io.IOUtils; //(For this you need to add "commons-io-1.3.1.jar" in your project.)
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
-
 import com.fasterxml.jackson.core.JsonParser;
-
 import net.minidev.json.JSONArray;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
@@ -36,16 +31,13 @@ import net.minidev.json.parser.ParseException;
 	* For XML output URL will be "http://maps.googleapis.com/maps/api/geocode/xml"; 
 	*/
 
-		 public static String[] getLatLongPositions(String address) throws Exception
+	 public static String[] getLatLongPositions(String address) throws Exception
          {
                    int responseCode = 0;
                    String api = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + URLEncoder.encode(address, "UTF-8") + "&sensor=true";
                    URL url = new URL(api);
-                  System.setProperty("http.proxyHost", "10.2.11.31");
-               System.setProperty("http.proxyPort", "8585");
-
-               	
-
+                   System.setProperty("http.proxyHost", "10.2.11.31");
+                   System.setProperty("http.proxyPort", "8585");
 
                    HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
 
